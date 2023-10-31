40d6ae7d2622b3253bc43a42ffb9271ce953282c
Tanvir Bhuiyan <mdtanvir95925@gmail.com>
2023-11-01 02:39:15 +0600

    Navbar added

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -37,7 +37,7 @@ const HeroContent = () => {
               {" "}
               the best{" "}
             </span>
-            project exprience
+            project experience
           </span>
         </motion.div>
         <motion.p
